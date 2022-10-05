06efda4566aebdb0af6128f8f4176ce6f4c40223
Trevor McLaurine <trevor.mclaurine@gmail.com>
2022-10-05 09:52:10 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      lightblue: 'hsl(211, 68%, 94%)',
+      white: 'hsl(0, 0%, 100%)',
+      darkblue: 'hsl(219, 85%, 26%)',
+      grey: 'hsl(219, 14%, 63%)',
+      charcoal: 'hsl(224, 21%, 14%)',
+      darkgrey: 'hsl(219, 12%, 42%)',
+      lightred: 'hsl(1, 90%, 64%)'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
